5e1d360532652dc251001f93d088503649ffde17
薛定谔的猫 <weiran.zsd@outlook.com>
2019-02-09 17:43:40 +0800

    Fix: should output the correct configs & rules (#5)

---
+++ tests/lib/index.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Tests for index.js
+ * @author 唯然 <weiran.zsd@outlook.com>
+ */
+
+"use strict";
+
+const entry = require("../../lib/index");
+const assert = require("assert");
+
+describe("should have the corrent entry", () => {
+    it("should export config: all", () => {
+        assert(entry && entry.configs && entry.configs.all);
+        assert(entry.configs.all.rules["no-debugger"], "off");
+        assert(entry.configs.all.rules["autofix/no-debugger"], "error");
+    });
+    it("should export all rules", () => {
+        const rule = entry.rules["no-debugger"];
+
+        assert(rule && rule.meta && rule.create);
+    });
+});
