08ee97e6fc61554528750bc310a9d8ada7e94997
weiran.zsd <weiran.zsd@alibaba-inc.com>
2019-02-09 17:02:25 +0800

    Chore: simplify entry tests

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -10,17 +10,13 @@ const assert = require("assert");
 
 describe("should have the corrent entry", () => {
     it("should export config: all", () => {
-        assert(typeof entry === "object");
-        assert(typeof entry.configs === "object");
-        assert(typeof entry.configs.all === "object");
+        assert(entry && entry.configs && entry.configs.all);
         assert(entry.configs.all["no-debugger"], "off");
         assert(entry.configs.all["autofix/no-debugger"], "error");
     });
     it("should export all rules", () => {
         const rule = entry.rules["no-debugger"];
 
-        assert(rule);
-        assert(rule.meta);
-        assert(rule.create);
+        assert(rule && rule.meta && rule.create);
     });
 });
