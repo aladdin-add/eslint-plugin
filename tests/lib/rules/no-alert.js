306cbd11eaa36583e51265676dd5e87e5cf5362c
Pig Fang <g-plane@hotmail.com>
2019-02-11 16:29:54 +0800

    New: add fixer for "no-alert"

---
+++ tests/lib/rules/no-alert.js
@@ -0,0 +1,60 @@
+/**
+ * @fileoverview Tests for rule no-alert
+ * @author Pig Fang <g-plane@hotmail.com>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-alert");
+const RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+const errors = [{ messageId: "unexpected", type: "CallExpression" }];
+
+ruleTester.run("no-alert", rule, {
+    valid: [
+        "alert",
+        "prompt",
+        "confirm"
+    ],
+    invalid: [
+        {
+            code: "alert('test')",
+            output: "",
+            errors
+        },
+        {
+            code: "alert(test)",
+            output: "",
+            errors
+        },
+        {
+            code: "prompt('test')",
+            output: "",
+            errors
+        },
+        {
+            code: "prompt(test)",
+            output: "",
+            errors
+        },
+        {
+            code: "confirm('test')",
+            output: "",
+            errors
+        },
+        {
+            code: "confirm(test)",
+            output: "",
+            errors
+        }
+    ]
+});
