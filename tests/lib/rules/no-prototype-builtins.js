5236cf79145a2fa1f3cbeedbec427188bee92616
Pig Fang <g-plane@hotmail.com>
2019-02-12 10:31:37 +0800

    New: add fixer for rule "no-prototype-builtins"

---
+++ tests/lib/rules/no-prototype-builtins.js
@@ -0,0 +1,47 @@
+/**
+ * @fileoverview Tests for rule no-prototype-builtins
+ * @author Pig Fang <g-plane@hotmail.com>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-prototype-builtins");
+const RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+const errors = [{ type: "CallExpression" }];
+
+ruleTester.run("no-prototype-builtins", rule, {
+    valid: [
+        "Object.prototype.hasOwnProperty.call(foo, \"bar\")"
+    ],
+    invalid: [
+        {
+            code: "foo.hasOwnProperty('bar')",
+            output: "Object.prototype.hasOwnProperty.call(foo, 'bar')",
+            errors
+        },
+        {
+            code: "foo.bar.hasOwnProperty('baz')",
+            output: "Object.prototype.hasOwnProperty.call(foo.bar, 'baz')",
+            errors
+        },
+        {
+            code: "foo.isPrototypeOf('bar')",
+            output: "Object.prototype.isPrototypeOf.call(foo, 'bar')",
+            errors
+        },
+        {
+            code: "foo.propertyIsEnumerable('bar')",
+            output: "Object.prototype.propertyIsEnumerable.call(foo, 'bar')",
+            errors
+        }
+    ]
+});
