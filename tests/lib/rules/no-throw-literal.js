5f24f415f448ed62fc2426174203fb3b0a3374a2
Pig Fang <g-plane@hotmail.com>
2019-02-25 09:43:30 +0800

    Add "no-throw-literal"

---
+++ tests/lib/rules/no-throw-literal.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview Tests for rule no-throw-literal.
+ * @author Pig Fang <g-plane@hotmail.com>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-throw-literal");
+const RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+const errors = [{ type: "ThrowStatement" }];
+
+ruleTester.run("no-throw-literal", rule, {
+    valid: [
+        "throw new Error()",
+        "throw error"
+    ],
+    invalid: [
+        {
+            code: "throw undefined",
+            output: "throw new Error();",
+            errors
+        },
+        {
+            code: "throw 'error'",
+            output: "throw new Error('error');",
+            errors
+        }
+    ]
+});
