332822cc15b89e00e19586af51c83a349e1a66c3
Pig Fang <g-plane@hotmail.com>
2019-06-08 11:47:22 +0800

    New: add "no-unused-vars" (#41)
    * New: add "no-unused-vars"
    
    * update tests
    
    * support simple variables declarations
    
    * support more patterns
    
    * add more cases

---
+++ tests/lib/rules/no-unused-vars.js
@@ -0,0 +1,190 @@
+/**
+ * @fileoverview Tests for rule no-unused-vars.
+ * @author Pig Fang <g-plane@hotmail.com>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-unused-vars");
+const RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-unused-vars", rule, {
+    valid: [
+        {
+            code: "import 'm'",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 }
+        }
+    ],
+    invalid: [
+        {
+            code: "import * as m from 'm'",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import m from 'm'",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import {m} from 'm'",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import {m1 as m2} from 'm'",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import m, {b} from 'm'; b;",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "import  {b} from 'm'; b;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import {a, b} from 'm'; b;",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "import { b} from 'm'; b;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import {a1 as a2, b} from 'm'; b;",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "import { b} from 'm'; b;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import {a, b} from 'm'; a;",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "import {a} from 'm'; a;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "import m, {a} from 'm'; m;",
+            parserOptions: { sourceType: "module", ecmaVersion: 6 },
+            output: "import m from 'm'; m;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = b",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = undefined",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = null",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = 'b'",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = this",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = `template`",
+            parserOptions: { ecmaVersion: 6 },
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = this.value",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = b = c",
+            output: "",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = `template-${value}`",
+            parserOptions: { ecmaVersion: 6 },
+            output: null,
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = b()",
+            output: null,
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = (b()).c",
+            output: null,
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = b = c()",
+            output: null,
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = b, c = d; c;",
+            output: "var  c = d; c;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "var a = b, c = d; a;",
+            output: "var a = b ; a;",
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "let {...a} = b",
+            output: "let {} = b",
+            parserOptions: { ecmaVersion: 2018 },
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "let {a} = b",
+            output: "let {} = b",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "let {a1: a2} = b",
+            output: "let {} = b",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "let {a = b} = c",
+            output: "let {} = c",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ type: "Identifier" }]
+        },
+        {
+            code: "let {a = b()} = c",
+            output: null,
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ type: "Identifier" }]
+        }
+    ]
+});
