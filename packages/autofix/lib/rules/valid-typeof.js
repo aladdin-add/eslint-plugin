2d6643dcbeb5866bd45e08151f6d0e161b329897
SukkaW <isukkaw@gmail.com>
2025-06-28 23:35:57 +0800

    refactor: drop deprecated deps

--- packages/autofix/lib/rules/valid-typeof.js
+++ packages/autofix/lib/rules/valid-typeof.js
@@ -6,7 +6,7 @@
 
 const ruleComposer = require("eslint-rule-composer");
 const utils = require("../utils");
-const stringSimilarity = require("string-similarity");
+const { closest } = require("fastest-levenshtein");
 
 const rule = utils.getFixableRule("valid-typeof", false);
 const VALID_TYPES = ["symbol", "undefined", "object", "boolean", "number", "string", "function"];
@@ -19,12 +19,10 @@ module.exports = ruleComposer.mapReports(
                 { parent } = node;
             const sibling = parent.left.type.endsWith("Literal") ? parent.left : parent.right;
             const value = sibling.type === "Literal" ? sibling.value : sibling.quasis[0].value.cooked;
-            const best = stringSimilarity.findBestMatch(value, VALID_TYPES).bestMatch;
+            const best = closest(value, VALID_TYPES);
 
-            if (best.rating > 0.3) {
-                return fixer.replaceText(sibling, `"${best.target}"`);
-            }
-            return null;
+            return fixer.replaceText(sibling, `"${best}"`);
+
         };
         return problem;
     }
